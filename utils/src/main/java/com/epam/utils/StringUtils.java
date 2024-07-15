cb6bbfe6a5104a2ea528d98db8c02356b2c8701c
Bohdan Rudenko <brudenko2804@gmail.com>
2024-07-15 22:31:08 +0300

    Change logic

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -2,11 +2,13 @@ package com.epam.utils;
 
 import org.apache.commons.lang3.math.NumberUtils;
 
+import java.math.BigDecimal;
+
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
         if (NumberUtils.isParsable(str)) {
-            int i = Integer.parseInt(str);
-            return i > 0;
+            BigDecimal number = new BigDecimal(str);
+            return number.compareTo(BigDecimal.ZERO) > 0;
         }
         return false;
     }
