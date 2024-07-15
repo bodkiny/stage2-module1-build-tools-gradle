aedb0d0a6c0c53370bb7c7330d07f16bb2a0fc85
Bohdan Rudenko <brudenko2804@gmail.com>
2024-07-15 21:47:12 +0300

    Change logic

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -6,11 +6,7 @@ import java.util.List;
 
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
-        for (String str : args) {
-            if (!StringUtils.isPositiveNumber(str)) {
-                return false;
-            }
-        }
-        return true;
+        if (args == null) return false;
+        return args.stream().allMatch(StringUtils::isPositiveNumber);
     }
 }
--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -1,15 +1,7 @@
 package com.epam.utils;
 
-import org.apache.commons.lang3.math.NumberUtils;
-
-import java.math.BigDecimal;
-
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-        if (NumberUtils.isParsable(str)) {
-            BigDecimal number = new BigDecimal(str);
-            return number.compareTo(BigDecimal.ZERO) > 0;
-        }
-        return false;
+        return org.apache.commons.lang3.StringUtils.isNumeric(str) && Integer.parseInt(str) > 0;
     }
 }
